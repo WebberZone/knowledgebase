fd2f25c26223349907df1038fba9f84ffb0653ca
Ajay D'Souza <me@ajaydsouza.com>
2015-05-17 22:07:46 +0100

    Support 1 or 2 levels of category

--- admin/admin.php
+++ admin/admin.php
@@ -6,7 +6,40 @@
  * @link       https://webberzone.com
  * @since      0.9.0
  *
- * @package    Wzkb
- * @subpackage Wzkb/admin
+ * @package    WZKB
+ * @subpackage WZKB/admin
  */
+
+
+
+/**
+ * Customise the taxonomy columns.
+ *
+ * @since	1.0.0
+ *
+ * @param	array	Admin Columns
+ * @return	array	Updated Admin Columns Array
+ */
+function wzkb_tax_columns( $new_columns ) {
+    $new_columns = array(
+        'cb' => '<input type="checkbox" />',
+        'name'   => __('Name'),
+        'slug'   => __('Slug'),
+        'posts'  => __('Posts'),
+        'tax_id' => 'ID',
+    );
+
+    return $new_columns;
+}
+add_filter( "manage_edit-wzkb_category_columns", 'wzkb_tax_columns' );
+add_filter( "manage_edit-wzkb_category_sortable_columns", 'wzkb_tax_columns' );
+add_filter( "manage_edit-wzkb_tag_columns", 'wzkb_tax_columns' );
+add_filter( "manage_edit-wzkb_tag_sortable_columns", 'wzkb_tax_columns' );
+
+
+function wzkb_tax_id( $value, $name, $id ) {
+    return 'tax_id' === $name ? $id : $value;
+}
+add_filter( "manage_wzkb_category_custom_column", 'wzkb_tax_id', 10, 3 );
+add_filter( "manage_wzkb_tag_custom_column", 'wzkb_tax_id', 10, 3 );
 
--- includes/shortcode.php
+++ includes/shortcode.php
@@ -16,67 +16,90 @@ function wzkb_knowledgebase() {
 
 
 	// Get Knowledge Base Sections
-	$kb_sections = get_terms( 'wzkb_category', array(
-	    'orderby'    => 'count',
+	$kb_master_sections = get_terms( 'wzkb_category', array(
+	    'orderby'    => 'name',
 	    'hide_empty' => 0,
-//		'parent' => 0,
+		'parent' => 0,
 	) );
 
 	$output = '';
 
 	$output .= '<div class="wzkb">';
 
-	if ( ! empty( $kb_sections ) && ! is_wp_error( $kb_sections ) ) {
+	if ( ! empty( $kb_master_sections ) && ! is_wp_error( $kb_master_sections ) ) {
+
+		foreach ( $kb_master_sections as $kb_master_section ) {
+
+			$kb_sections = get_terms( 'wzkb_category', array(
+			    'orderby'    => 'name',
+			    'hide_empty' => 0,
+				'child_of' => $kb_master_section->term_id,
+			) );
+
+			$output .= '<div class="wzkb_master_section">';
+			$output .= '<h4 class="wzkb-master-section-name">
+							<a href="' . get_term_link( $kb_master_section ) . '" title="' . $kb_master_section->name . '" >' . $kb_master_section->name . '</a>
+						</h4>';
 
 
-		// For each knowledge base section
-		foreach ( $kb_sections as $section ) {
+			if ( ! empty( $kb_sections ) && ! is_wp_error( $kb_sections ) ) {
 
-			$output .= '<div class="wzkb_section">';
 
-			// Display Section Name
-//			$output .= '<h4 class="wzkb-section-name"><a href="' . get_term_link( $section ) . '" title="' . $section->name . '" ><span class="dashicons dashicons-category"></span>' . $section->name . '</a></h4>';
-			$output .= '<h4 class="wzkb-section-name">' . $section->name . '</h4>';
+				// For each knowledge base section
+				foreach ( $kb_sections as $section ) {
 
-			$output .= '<ul class="wzkb-articles-list">';
+					$output .= '<div class="wzkb_section">';
 
-			// Fetch posts in the section
-			$kb_args = array(
-				'post_type' => 'wz_knowledgebase',
-				'posts_per_page'=> 5,
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'wzkb_category',
-						'terms'    => $section,
-					),
-				),
-			);
+					// Display Section Name
+		//			$output .= '<h4 class="wzkb-section-name"><a href="' . get_term_link( $section ) . '" title="' . $section->name . '" ><span class="dashicons dashicons-category"></span>' . $section->name . '</a></h4>';
+					$output .= '<h4 class="wzkb-section-name">' . $section->name . '</h4>';
 
-			$query = new WP_Query( $kb_args );
+					// Fetch posts in the section
+					$kb_args = array(
+						'post_type' => 'wz_knowledgebase',
+						'posts_per_page'=> 5,
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'wzkb_category',
+								'terms'    => $section,
+								'include_children' => false,
+							),
+						),
+					);
 
-			if ( $query->have_posts() ) :
+					$query = new WP_Query( $kb_args );
 
-				while ( $query->have_posts() ) : $query->the_post();
+					if ( $query->have_posts() ) :
 
-					$output .=  '<li class="wzkb-article-name">';
-					$output .=  '<a href="'. get_permalink( get_the_ID() ) .'" rel="bookmark" title="'. get_the_title( get_the_ID() ) .'">'. get_the_title( get_the_ID() ) .'</a>';
-					$output .=  '</li>';
+						$output .= '<ul class="wzkb-articles-list">';
 
-				endwhile;
+						while ( $query->have_posts() ) : $query->the_post();
 
-				wp_reset_postdata();
+							$output .=  '<li class="wzkb-article-name">';
+							$output .=  '<a href="'. get_permalink( get_the_ID() ) .'" rel="bookmark" title="'. get_the_title( get_the_ID() ) .'">'. get_the_title( get_the_ID() ) .'</a>';
+							$output .=  '</li>';
 
-			else :
+						endwhile;
 
-				$output .= '<p>No Articles Found</p>';
+						$output .=  '</ul>';
 
-			endif;
+						wp_reset_postdata();
 
-			$output .=  '</ul></div>';
+					else :
 
+						$output .= '<p>No Articles Found</p>';
+
+					endif;
+
+					$output .=  '</div>';	// End wzkb_section
+
+
+				}
+			}
+
+			$output .= '</div>';	// End wzkb_master_section
 
 		}
-
 	}
 
 	$output .= '<div class="wzkb_clear"></div>';
