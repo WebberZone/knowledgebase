ee3eda2034121e026673a16ece3ac398779c15b7
Ajay D'Souza <me@ajaydsouza.com>
2015-05-23 18:40:07 +0100

    Cleaned up styles

--- includes/main.php
+++ includes/main.php
@@ -21,193 +21,6 @@ if ( ! defined( 'WPINC' ) ) {
  *
  * @since	1.0.0
  *
- * @param	array	$args		Parameters array
- * @return	$output	Formatted shortcode output
- */
-function wzkb_knowledge2( $args = array() ) {
-
-	$defaults = array(
-		'category' => FALSE,		// Create a knowledgebase for subcategories of this parent ID
-	);
-
-	// Parse incomming $args into an array and merge it with $defaults
-	$args = wp_parse_args( $args, $defaults );
-
-	// Set parent category if defined
-	$parent = ( 0 < $args['category'] ) ? ( $args['category'] ) : 0;
-
-	$output = '';
-
-	$output .= '<div class="wzkb">';
-
-	// Get Knowledge Base Sections
-	$kb_master_sections = get_terms( 'wzkb_category', array(
-	    'orderby'    => 'name',
-	    'hide_empty' => 0,
-		'parent' => $parent,
-	) );
-
-	if ( ! empty( $kb_master_sections ) && ! is_wp_error( $kb_master_sections ) ) {
-
-		foreach ( $kb_master_sections as $kb_master_section ) {
-
-			$kb_sections = get_terms( 'wzkb_category', array(
-			    'orderby'    => 'name',
-			    'hide_empty' => 0,
-				'child_of' => $kb_master_section->term_id,
-			) );
-
-
-			if ( ! empty( $kb_sections ) && ! is_wp_error( $kb_sections ) ) {
-
-				$output .= '<div class="wzkb_master_section">';
-
-				$output .= '<h4 class="wzkb-master-section-name">
-								<a href="' . get_term_link( $kb_master_section ) . '" title="' . $kb_master_section->name . '" >' . $kb_master_section->name . '</a>
-							</h4>';
-
-				// For each knowledge base section
-				foreach ( $kb_sections as $section ) {
-
-					$section_children = get_terms( 'wzkb_category', array(
-					    'orderby'    => 'name',
-					    'hide_empty' => 0,
-						'child_of' => $section->term_id,
-					) );
-					if ( ! empty( $section_children ) && ! is_wp_error( $section_children ) ) {
-						$section_children = wp_list_pluck( $section_children, 'term_id' );
-					} else {
-						$section_children = array();
-					}
-
-
-					$output .= '<div class="wzkb_section">';
-
-					// Display Section Name
-					$output .= '<h4 class="wzkb-section-name">' . $section->name . '</h4>';
-
-					// Fetch posts in the section
-					$kb_args = array(
-						'post_type' => 'wz_knowledgebase',
-						'posts_per_page'=> 5,
-						'tax_query' => array(
-							'relation' => 'AND',
-							array(
-								'taxonomy' => 'wzkb_category',
-								'terms'    => $section,
-								'include_children' => false,
-							),
-							array(
-								'taxonomy' => 'wzkb_category',
-								'field'    => 'term_id',
-								'terms'    => $section_children,
-								'operator' => 'NOT IN',
-							),
-						),
-					);
-
-					$query = new WP_Query( $kb_args );
-
-					if ( $query->have_posts() ) :
-
-						$output .= '<ul class="wzkb-articles-list">';
-
-						while ( $query->have_posts() ) : $query->the_post();
-
-							$output .=  '<li class="wzkb-article-name">';
-							$output .=  '<a href="'. get_permalink( get_the_ID() ) .'" rel="bookmark" title="'. get_the_title( get_the_ID() ) .'">'. get_the_title( get_the_ID() ) .'</a>';
-							$output .=  '</li>';
-
-						endwhile;
-
-						$output .=  '</ul>';
-
-						$output .= '<p class="wzkb-article-footer">' . __( "Read more articles in ", 'wzkb' ) . '
-										<a href="' . get_term_link( $section ) . '" title="' . $section->name . '" >' . $section->name . '</a> &raquo;
-									</p>';
-
-						wp_reset_postdata();
-
-					else :
-
-						$output .= '<p>No Articles Found</p>';
-
-					endif;
-
-					$output .=  '</div>';	// End wzkb_section
-
-
-				}
-
-				$output .= '</div>';	// End wzkb_master_section
-
-			} else {
-
-					$output .= '<div class="wzkb_section">';
-
-					// Display Section Name
-					$output .= '<h4 class="wzkb-section-name">' . $kb_master_section->name . '</h4>';
-
-					// Fetch posts in the section
-					$kb_args = array(
-						'post_type' => 'wz_knowledgebase',
-						'posts_per_page'=> 5,
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'wzkb_category',
-								'terms'    => $kb_master_section,
-								'include_children' => false,
-							),
-						),
-					);
-
-					$query = new WP_Query( $kb_args );
-
-					if ( $query->have_posts() ) :
-
-						$output .= '<ul class="wzkb-articles-list">';
-
-						while ( $query->have_posts() ) : $query->the_post();
-
-							$output .=  '<li class="wzkb-article-name">';
-							$output .=  '<a href="'. get_permalink( get_the_ID() ) .'" rel="bookmark" title="'. get_the_title( get_the_ID() ) .'">'. get_the_title( get_the_ID() ) .'</a>';
-							$output .=  '</li>';
-
-						endwhile;
-
-						$output .=  '</ul>';
-
-						$output .= '<p class="wzkb-article-footer">' . __( "Read more articles in ", 'wzkb' ) . '
-										<a href="' . get_term_link( $kb_master_section ) . '" title="' . $kb_master_section->name . '" >' . $kb_master_section->name . '</a> &raquo;
-									</p>';
-
-						wp_reset_postdata();
-
-					else :
-
-						$output .= '<p>No Articles Found</p>';
-
-					endif;
-
-					$output .=  '</div>';	// End wzkb_section
-
-			}
-
-		}
-	}
-
-	$output .= '<div class="wzkb_clear"></div>';
-	$output .= "</div>";
-
-	return apply_filters( 'wzkb_knowledge', $output, $args );
-}
-
-
-/**
- * The main function to generate the output.
- *
- * @since	1.0.0
- *
  * @param	array	$args	Parameters array
  * @return	$output	Formatted shortcode output
  */
@@ -277,6 +90,8 @@ function wzkb_looper( $term, $level, $processed = false ) {
 		'parent' => $term->term_id,
 	) );
 
+	$loop = 0;
+
 	$output = '';
 
 	if ( ! empty( $children ) && ! is_wp_error( $children ) ) {
@@ -318,6 +133,8 @@ function wzkb_looper( $term, $level, $processed = false ) {
 
 				$query = new WP_Query( $kb_args );
 
+				$count = count( $query );
+
 				if ( $query->have_posts() ) :
 
 					// Display Section Name
@@ -339,7 +156,7 @@ function wzkb_looper( $term, $level, $processed = false ) {
 
 					$output .= wzkb_looper( $child, $level + 1, true );
 
-					if ( $level < 2 ) {
+					if ( ( $level < 2 ) && ( $query->found_posts > 5 ) ) {
 
 						$output .= '<p class="wzkb-article-footer">' . __( "Read more articles in ", 'wzkb' ) . '
 										<a href="' . get_term_link( $child ) . '" title="' . $child->name . '" >' . $child->name . '</a> &raquo;
