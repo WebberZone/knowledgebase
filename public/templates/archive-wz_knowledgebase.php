fc8166eefca1eb1c8aae73884ffb2633fdbb9176
Ajay D'Souza <me@ajaydsouza.com>
2016-01-03 12:41:27 +0000

    Fixed display in the archive template

--- public/templates/archive-wz_knowledgebase.php
+++ public/templates/archive-wz_knowledgebase.php
@@ -14,13 +14,8 @@
 /* This plugin uses the Archive file of TwentyFifteen theme as an example */
 get_header();
 
-// Hide the first level header when displaying archives
-$custom_css = '
-    .wzkb-section-name-level-1 {
-        display: none;
-    }
-';
-wp_add_inline_style( 'wzkb_styles', $custom_css );
+wp_enqueue_style( 'wzkb_styles' );
+
 ?>
 
 	<section id="primary" class="content-area">
@@ -37,7 +32,7 @@ wp_add_inline_style( 'wzkb_styles', $custom_css );
 			<?php
 				wzkb_get_search_form();
 
-				echo do_shortcode( '[knowledgebase]' );
+				echo wzkb_knowledge();
 
 			// If no content, include the "No posts found" template.
 		else :
