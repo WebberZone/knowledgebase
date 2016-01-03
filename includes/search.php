5c031abab15cecb90c471a3e0f100d8f969ee4ae
Ajay D'Souza <me@ajaydsouza.com>
2016-01-03 12:41:37 +0000

    Fixed text domain

--- includes/search.php
+++ includes/search.php
@@ -29,11 +29,11 @@ function wzkb_get_search_form( $echo = true ) {
 	$form = '
     <form role="search" method="get" class="wzkb-search-form" action="' . esc_url( home_url( '/' ) ) . '">
         <label>
-            <span class="screen-reader-text">' . _x( 'Search for:', 'label', 'wzkb' ) . '</span>
-            <input type="search" class="wzkb-search-field" placeholder="' . esc_attr_x( 'Search the knowledgebase &hellip;', 'placeholder', 'wzkb' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label', 'wzkb' ) . '" />
+            <span class="screen-reader-text">' . _x( 'Search for:', 'label', 'knowledgebase' ) . '</span>
+            <input type="search" class="wzkb-search-field" placeholder="' . esc_attr_x( 'Search the knowledgebase &hellip;', 'placeholder', 'knowledgebase' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label', 'knowledgebase' ) . '" />
         </label>
         <input type="hidden" name="post_type" value="wz_knowledgebase">
-        <input type="submit" class="wzkb-search-submit" value="'. esc_attr_x( 'Search', 'submit button', 'wzkb' ) .'" />
+        <input type="submit" class="wzkb-search-submit" value="'. esc_attr_x( 'Search', 'submit button', 'knowledgebase' ) .'" />
     </form>
     ';
 
@@ -47,11 +47,14 @@ function wzkb_get_search_form( $echo = true ) {
 	$result = apply_filters( 'wzkb_get_search_form', $form );
 
 	if ( null === $result ) {
-		$result = $form; }
+		$result = $form;
+	}
 
 	if ( $echo ) {
 		echo $result;
-	} else { 		return $result; }
+	} else {
+		return $result;
+	}
 }
 
 
