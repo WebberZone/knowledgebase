af9bc7a4774524798b927d3a55afe53e0c8e421e
Ajay DSouza <me@ajaydsouza.com>
2016-12-03 16:51:33 +0000

    Delete the custom taxonomy

--- uninstall.php
+++ uninstall.php
@@ -34,6 +34,11 @@ if ( is_multisite() ) {
 }
 
 
+/**
+ * Delete Data.
+ *
+ * @since 1.3.0
+ */
 function wzkb_delete_data() {
 
 	$settings = get_option( 'wzkb_settings' );
@@ -51,7 +56,33 @@ function wzkb_delete_data() {
 		foreach ( $wzkbs as $wzkb ) {
 			wp_delete_post( $wzkb->ID );
 		}
+
+		wzkb_delete_taxonomy( 'wzkb_category' );
+		wzkb_delete_taxonomy( 'wzkb_tag' );
 	}
 
 }
 
+
+/**
+ * Delete Custom Taxonomy.
+ *
+ * @since 1.3.0
+ *
+ * @param string $taxonomy Custom taxonomy.
+ */
+function wzkb_delete_taxonomy( $taxonomy ) {
+	global $wpdb;
+
+	$query = 'SELECT t.name, t.term_id
+            FROM ' . $wpdb->terms . ' AS t
+            INNER JOIN ' . $wpdb->term_taxonomy . ' AS tt
+            ON t.term_id = tt.term_id
+            WHERE tt.taxonomy = "' . $taxonomy . '"';
+
+	$terms = $wpdb->get_results( $query );
+
+	foreach ( $terms as $term ) {
+		wp_delete_term( $term->term_id, $taxonomy );
+	}
+}
