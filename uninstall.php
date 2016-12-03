0882d8d1de032712666f8fa5a3944397ad58674c
Ajay DSouza <me@ajaydsouza.com>
2016-12-03 16:33:19 +0000

    Multisite support

--- uninstall.php
+++ uninstall.php
@@ -14,20 +14,44 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-$settings = get_option( 'wzkb_settings' );
+global $wpdb;
 
-if ( $settings['uninstall_options'] ) {
+if ( is_multisite() ) {
 
-	delete_option( 'wzkb_settings' );
+	// Get all blogs in the network and activate plugin on each one.
+	$blog_ids = $wpdb->get_col( "
+		SELECT blog_id FROM $wpdb->blogs
+		WHERE archived = '0' AND spam = '0' AND deleted = '0'
+	" );
 
+	foreach ( $blog_ids as $blog_id ) {
+		switch_to_blog( $blog_id );
+		wzkb_delete_data();
+		restore_current_blog();
+	}
+} else {
+	wzkb_delete_data();
 }
 
-if ( $settings['uninstall_data'] ) {
 
-	$wzkbs = get_posts( array( 'post_type' => 'products' ) );
+function wzkb_delete_data() {
 
-	foreach ( $wzkbs as $wzkb ) {
-		wp_delete_post( $wzkb->ID );
+	$settings = get_option( 'wzkb_settings' );
+
+	if ( $settings['uninstall_options'] ) {
+
+		delete_option( 'wzkb_settings' );
+
 	}
+
+	if ( $settings['uninstall_data'] ) {
+
+		$wzkbs = get_posts( array( 'post_type' => 'wz_knowledgebase' ) );
+
+		foreach ( $wzkbs as $wzkb ) {
+			wp_delete_post( $wzkb->ID );
+		}
+	}
+
 }
 
