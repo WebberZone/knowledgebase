9c77f008c773d9ef5bb2c04cacd0bd87ad47e6c6
Ajay DSouza <me@ajaydsouza.com>
2017-07-29 11:09:37 +0100

    Updated tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,34 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package WHEREGO
+ */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
-	include dirname( __FILE__ ) . '/../knowledgebase.php';
+	require dirname( dirname( __FILE__ ) ) . '/knowledgebase.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+echo dirname( dirname( __FILE__ ) ) . '/knowledgebase.php';
 
 activate_plugin( 'knowledgebase/knowledgebase.php' );
 
 echo "Installing Knowledgebase...\n";
 
+global  $wherego_settings, $current_user;
+$wherego_settings = wherego_get_settings();
