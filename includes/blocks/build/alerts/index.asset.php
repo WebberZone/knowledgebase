d60a9c28ed53f41fe2eee92234f565317a25f0c4
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2025-09-17 20:59:51 +0400

    Fixed colour contrast
    Fixes #64

--- includes/blocks/build/alerts/index.asset.php
+++ includes/blocks/build/alerts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7034904210e64b0a502f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e4f02cbc6cf576853ca5');
